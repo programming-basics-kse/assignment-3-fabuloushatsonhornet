2bb7c85ebee2ae64efc95cd9a334acc897ea617f
OBudiak <obudiak@kse.org.ua>
2024-11-22 18:42:40 +0200

    bug fixed, updating functionality

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import pycountry
 from pycountry import countries
 
 parser = argparse.ArgumentParser('Olympic data base')
-parser.add_argument('data_base', type=str, default='data_base_olympic.tsv')
+# parser.add_argument('input file', default='main.py')
+parser.add_argument('data_base', type=str)
 # parser.add_argument('-medals', type=str)
 # parser.add_argument('-output', type=str, default='')
 # parser.add_argument('-total', type=int)
-parser.add_argument('-overall', type=str)
+parser.add_argument('-overall', nargs='+', type=str)
 # parser.add_argument('-interactive', type=bool, default=False)
 args = parser.parse_args()
 
@@ -25,14 +26,16 @@ def get_country_code(country_n):
 
 def overall(code):
     with open(args.data_base, 'r') as file:
-        line = file.readline()
+        line = file.readline()[:-1].split('\t')
         header = line #створив на всякий випадок, якщо доведеться діставати індекси
         i_year = 9
         i_medal = -1
         i_country = 7
         country_stats = {}
-        while line:
-            line = file.readline()
+        while True:
+            line = file.readline()[:-1].split('\t')
+            if line == ['']:
+                break
             if code != line[i_country] or line[i_medal] == 'NA':
                 continue
             year = line[i_year]
@@ -44,6 +47,7 @@ def overall(code):
 
 
 countries_overall = {}
+args.overall = tuple(args.overall)
 for country in args.overall:
     country_code = get_country_code(country)
     print(country_code)

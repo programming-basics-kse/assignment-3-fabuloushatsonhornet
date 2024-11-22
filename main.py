1c78f2e7fae9f34f55f7728deacb47b1215d1a2e
OBudiak <obudiak@kse.org.ua>
2024-11-22 17:48:20 +0200

    function overall added, testing in procces

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from pycountry import countries
 
 parser = argparse.ArgumentParser('Olympic data base')
 parser.add_argument('data_base', type=str, default='data_base_olympic.tsv')
-parser.add_argument('-medals', type=str)
-parser.add_argument('-output', type=str, default='')
-parser.add_argument('-total', type=int)
+# parser.add_argument('-medals', type=str)
+# parser.add_argument('-output', type=str, default='')
+# parser.add_argument('-total', type=int)
 parser.add_argument('-overall', type=str)
-parser.add_argument('-interactive', type=bool, default=False)
+# parser.add_argument('-interactive', type=bool, default=False)
 args = parser.parse_args()
 
 
@@ -21,14 +21,31 @@ def get_country_code(country_n):
     if country:
         return country_n.upper()
     else:
-        None
+        return None
+
+def overall(code):
+    with open(args.data_base, 'r') as file:
+        line = file.readline()
+        header = line #створив на всякий випадок, якщо доведеться діставати індекси
+        i_year = 9
+        i_medal = -1
+        i_country = 7
+        country_stats = {}
+        while line:
+            line = file.readline()
+            if code != line[i_country] or line[i_medal] == 'NA':
+                continue
+            year = line[i_year]
+            if year in country_stats:
+                country_stats[year] += 1
+            else:
+                country_stats[year] = 1
+    return country_stats
+
 
 countries_overall = {}
 for country in args.overall:
-    countries_overall[get_country_code(country)] = {'year':None, 'count':0}
-
-with open('data_base_olympic.tsv', 'r') as file:
-    next_line = file.readline()
-    header = next_line
-    while next_line:
-        next_line = file.readline()
+    country_code = get_country_code(country)
+    print(country_code)
+    print(overall(country_code))
+    # countries_overall[country_code] = {'year':None, 'count':0}

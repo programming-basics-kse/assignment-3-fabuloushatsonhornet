80d0ffc3197a7345c485994dcca8c7957bf4607f
OBudiak <obudiak@kse.org.ua>
2024-11-22 15:39:04 +0200

    parser added, still testing

---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser('Olympic data base')
+parser.add_argument('data_base', type=str, default='data.csv')
+parser.add_argument('-medals', type=str)
+parser.add_argument('country', type=str)
+parser.add_argument('year', type=int)
+parser.add_argument('-output', type=str, default='output.txt')
+parser.add_argument('-total', type=int)
+parser.add_argument('-overall', type=str)
+parser.add_argument('-interactive', type=bool, default=False)
+args = parser.parse_args()

4d9f8cc6980f2cdf3eb50f43fc4dd0e776b022c9
OBudiak <obudiak@kse.org.ua>
2024-11-22 15:41:11 +0200

    extra bug fix

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import argparse
 parser = argparse.ArgumentParser('Olympic data base')
 parser.add_argument('data_base', type=str, default='data.csv')
 parser.add_argument('-medals', type=str)
-parser.add_argument('country', type=str)
-parser.add_argument('year', type=int)
 parser.add_argument('-output', type=str, default='output.txt')
 parser.add_argument('-total', type=int)
 parser.add_argument('-overall', type=str)

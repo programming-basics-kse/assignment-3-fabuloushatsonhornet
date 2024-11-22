b3c03d4429dccd62ef20c13c159efc212f38e89b
OBudiak <obudiak@kse.org.ua>
2024-11-22 15:54:42 +0200

    bug fixed parser, file reader testing

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import argparse
+import csv
 
 parser = argparse.ArgumentParser('Olympic data base')
-parser.add_argument('data_base', type=str, default='data.csv')
+parser.add_argument('data_base', type=str, default='data_base_olympic.tsv')
 parser.add_argument('-medals', type=str)
-parser.add_argument('-output', type=str, default='output.txt')
+parser.add_argument('-output', type=str, default='')
 parser.add_argument('-total', type=int)
 parser.add_argument('-overall', type=str)
 parser.add_argument('-interactive', type=bool, default=False)
 args = parser.parse_args()
+
+with open('data_base_olympic.tsv', 'r') as file:
+    rows = []
+    csv_reader = csv.reader(file, delimiter=',')
+    header = next(csv_reader)
+    for row in csv_reader:
+        rows.append(row)

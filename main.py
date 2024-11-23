b4d1353501cb44b0de40996e8bf73278caa27495
OBudiak <obudiak@kse.org.ua>
2024-11-23 20:21:48 +0200

    first class version added

--- main.py
+++ main.py
@@ -14,42 +14,51 @@ parser.add_argument('-overall', nargs='+', type=str)
 args = parser.parse_args()
 
 
-def get_country_code(country_n):
-    for country in pycountry.countries:
-        if country.name.lower() == country_n.lower():
-            return country.alpha_3
-    country = pycountry.countries.get(alpha_3=country_n)
-    if country:
-        return country_n.upper()
-    else:
-        return None
+def output_console(arg_country):
+    counter = 0
+    for country in arg_country:
+        country_code = Overall.get_country_code(country)
+        if country_code == None:
+            continue
+        print(f"{country_code} - {Overall.overall(country_code)[0][0]} : {Overall.overall(country_code)[0][1]} medals")
+        counter += 1
 
-def overall(code):
-    with open(args.data_base, 'r') as file:
-        line = file.readline()[:-1].split('\t')
-        header = line #створив на всякий випадок, якщо доведеться діставати індекси
-        i_year = 9
-        i_medal = -1
-        i_country = 7
-        country_stats = {}
-        while True:
+    if counter == 0:
+        print("No information available...")
+
+
+class Overall:
+    def get_country_code(country_n):
+        for country in pycountry.countries:
+            if country.name.lower() == country_n.lower():
+                return country.alpha_3
+        country = pycountry.countries.get(alpha_3=country_n)
+        if country:
+            return country_n.upper()
+        else:
+            return None
+
+    def overall(code):
+        with open(args.data_base, 'r') as file:
             line = file.readline()[:-1].split('\t')
-            if line == ['']:
-                break
-            if code != line[i_country] or line[i_medal] == 'NA':
-                continue
-            year = line[i_year]
-            if year in country_stats:
-                country_stats[year] += 1
-            else:
-                country_stats[year] = 1
-    return sorted(country_stats.items(), key = lambda x: x[1], reverse = True)
+            header = line #створив на всякий випадок, якщо доведеться діставати індекси
+            i_year = 9
+            i_medal = -1
+            i_country = 7
+            country_stats = {}
+            while True:
+                line = file.readline()[:-1].split('\t')
+                if line == ['']:
+                    break
+                if code != line[i_country] or line[i_medal] == 'NA':
+                    continue
+                year = line[i_year]
+                if year in country_stats:
+                    country_stats[year] += 1
+                else:
+                    country_stats[year] = 1
+        return sorted(country_stats.items(), key = lambda x: x[1], reverse = True)
 
 
-
-countries_overall = {}
 args.overall = tuple(args.overall)
-for country in args.overall:
-    country_code = get_country_code(country)
-    print(f"{country_code} - {overall(country_code)[0][0]} : {overall(country_code)[0][1]} medals")
-    # countries_overall[country_code] = {'year':None, 'count':0}
+output_console(args.overall)

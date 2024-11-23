2ec2eb40a40355e23822c1d735d18376347ea282
Savelii <s.kozlov@kse.org.ua>
2024-11-23 15:37:23 +0200

    completed first task

--- main.py
+++ main.py
@@ -25,47 +25,58 @@ def validation_number(value):
         return value
     except ValueError:
         return False
-def validation_medal(value, year):
+
+def get_medal_and_country(value, year):
     try:
-        if (value[14].lower().capitalize().strip() == 'Gold' or value[14].lower().capitalize().strip() == 'Silver' or value[14].lower().capitalize().strip() == 'Bronze') and (data[9] == year):
+        if (value[14].lower().capitalize().strip() == 'Gold' or value[14].lower().capitalize().strip() == 'Silver' or value[14].lower().capitalize().strip() == 'Bronze') and (value[9] == year) and ((value[6].lower() == country.lower()) or (value[7].lower() == country.lower())):
             value[14] = value[14][:-1]
             return value
         return False
     except ValueError:
         return False
 
-def sort_by_date(data):
-    if data[9] == year:
-        return data
+
+def condition_checker(value):
+    if len(value) == 0:
+        return f"There was not any olympic games in this year."
+    if len(value) < 10:
+        return f"Ths country got less than 10 medals."
+    else:
+        return False
+
+
+def get_list(filename, year):
+    with open(f"{filename}", 'r') as file:
+        next_line = file.readline()
+        while next_line:
+            next_line = file.readline()
+            data = next_line.split('\t')
+            try:
+                if get_medal_and_country(data, year):
+                    filtered_data_list.append([data[0], data[1], data[6], data[7], data[8], data[9], data[13], data[14]])
+            except IndexError:
+                break
+
+def print_result(data_list, country, year):
+    if not condition_checker(data_list):
+        index = 1
+        print(f"Data for {year} olympic games. Team:{country}.")
+        for data in data_list:
+            name = data[1]
+            discipline = data[6]
+            medal = data[7]
+            print(f"{index}. {name} - {discipline} - {medal}")
+            index += 1
+    else:
+        print(condition_checker(data_list))
 
 year = '1998'
 country = 'USA'
 
 filtered_data_list = []
+get_list('Olympic Athletes - athlete_events.tsv', year)
 
-with open('Olympic Athletes - athlete_events.tsv', 'r') as file:
-    next_line = file.readline()
-    while next_line:
-        next_line = file.readline()
-        data = next_line.split('\t')
-        try:
-            if validation_medal(data, year):
-                filtered_data_list.append(data)
-        except IndexError:
-            break
+filtered_data_list = sorted(filtered_data_list, key=lambda x: int(x[0]), reverse=False)
 
 
-with open('Olympic Athletes - athlete_events.tsv', 'r') as file:
-    next_line = file.readline()
-    while next_line:
-        next_line = file.readline()
-        data = next_line.split('\t')
-        try:
-            if validation_medal(data, year):
-                filtered_data_list.append(data)
-        except IndexError:
-            break
-
-
-for data in filtered_data_list:
-    print(data)
+print_result(filtered_data_list, country, year)

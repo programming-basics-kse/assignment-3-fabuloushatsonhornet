b2103be1a314ffc6afa2e50a22a5d807360bfb81
Savelii <s.kozlov@kse.org.ua>
2024-11-24 21:00:07 +0200

    added total argument

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 import argparse
-import csv
 
 parser = argparse.ArgumentParser('Olympic data base')
 parser.add_argument('data_base', type=str, default='Olympic Athletes - athlete_events.tsv')
 parser.add_argument('-medals', type=str, nargs='+')
 # # parser.add_argument('-output', type=str, default='')
-parser.add_argument('-total', type=int, default=-1)
+parser.add_argument('-total', type=str, default=-1)
 # # parser.add_argument('-overall', type=str)
 # # parser.add_argument('-interactive', type=bool, default=False)
 args = parser.parse_args()
-#
-#
 data_base = args.data_base
-# country = args.country
-'''if not args.total:
-    year = args.total
-    total = True
+if args.total == -1:
+    total = False
+    year = args.medals[1]
+    country = args.medals[0]
 else:
-    year = args.year
-    total = False'''
-year = args.total # Тут просто с тотала достаеться сразу год, Дефолтом Я тебя запутал, теперь если тотала нету то по умолчанию стоит -1, это для проверки можно юзать
-# и аргумента со страной нету, это мой тупняк добавлять его, надо посмотреть откуда ты его должен доставать
-# !!! в medals храниться страна и год для которой нужно сделать поиск, все в str
-# Temporary
-
-def validation_number(value):
-    try:
-        value = float(value)
-        return value
-    except ValueError:
-        return False
+    total = True
+    year = args.total
+year_country = args.medals
 
 def get_medal_and_country(value, year):
         try:
@@ -65,25 +52,47 @@ def get_list(filename, year):
             data = next_line.split('\t')
             try:
                 if get_medal_and_country(data, year):
-                    filtered_data_list.append([data[0], data[1], data[6], data[7], data[8], data[9], data[13], data[14]])
+                    if total is False:
+                        filtered_data_list.append([data[0], data[1], data[6], data[7], data[8], data[9], data[13], data[14]])
+                    if total is True:
+                        filtered_data_list.append([data[6], data[14]])
             except IndexError:
                 break
     return filtered_data_list
 
-def print_result(data_list, country, year):
+def print_with_total(data_list):
+    data_dict = {}
+    for country, medal in data_list:
+        if country not in data_dict:
+            data_dict[country] = {'gold': 0, 'silver': 0, 'bronze': 0}
+        data_dict[country][medal.lower()] += 1
+    for country, medal in data_dict.items():
+        print(f"{country} - gold:{medal['gold']} - silver:{medal['silver']} - bronze:{medal['bronze']}")
+
+def print_without_total(data_list):
+    index = 1
+    print(f"Data for {year} olympic games. Team:{country}.")
+    for data in data_list:
+        name = data[1]
+        discipline = data[6]
+        medal = data[7]
+        print(f"{index}. {name} - {discipline} - {medal}")
+        index += 1
+
+def print_result(data_list, country ='', year=''):
     if not condition_checker(data_list):
-        index = 1
-        print(f"Data for {year} olympic games. Team:{country}.")
-        for data in data_list:
-            name = data[1]
-            discipline = data[6]
-            medal = data[7]
-            print(f"{index}. {name} - {discipline} - {medal}")
-            index += 1
+        if total is False:
+            print_without_total(data_list)
+        if total is True:
+            print_with_total(data_list)
     else:
         print(condition_checker(data_list))
 
 def main():
-    filtered_data_list = sorted(get_list(f"{data_base}", year), key=lambda x: int(x[0]), reverse=False)
-    print_result(filtered_data_list, country, year)
+    if total is False:
+        filtered_data_list = sorted(get_list(f"{data_base}", year), key=lambda x: int(x[0]), reverse=False)
+        print_result(filtered_data_list, country, year)
+    if total is True:
+        filtered_data_list = get_list(f"{data_base}", year)
+        print_result(filtered_data_list)
 main()

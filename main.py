480ab01a0159d3ff8f0e17358a51824980194111
main <s.kozlov@kse.org.ua>
2024-11-25 14:12:01 +0200

    added part fith classes

--- main.py
+++ main.py
@@ -10,81 +10,88 @@ parser.add_argument('-overall', nargs='+', type=str)
 parser.add_argument('-interactive', type=bool, default=False)
 args = parser.parse_args()
 data_base = args.data_base
+# -----
+import argparse
+class OlympicDataBase:
+    def __init__(self, data_base, medals=None, total=-1):
+        self.data_base = data_base
+        if total != -1:
+            self.total = True
+            self.year = total
+            self.country = None
+        else:
+            self.total = False
+            self.year = medals[1]
+            self.country = medals[0]
 
-if args.total == -1:
-    total = False
-    year = args.medals[1]
-    country = args.medals[0]
-else:
-    total = True
-    year = args.total
-
-def get_medal_and_country(value, year):
-    if not (value[14].lower().capitalize().strip() in ('Gold', 'Silver', 'Bronze') and value[9] == year):
-        return False
-    if total is False:
-        if country.lower() not in (value[6].lower(), value[7].lower()):
+    def get_medal_and_country(self, value):
+        if not (value[14].lower().capitalize().strip() in ('Gold', 'Silver', 'Bronze') and value[9] == self.year):
             return False
-    value[14] = value[14][:-1]
-    return value
+        if not self.total and self.country.lower() not in (value[6].lower(), value[7].lower()):
+            return False
+        value[14] = value[14][:-1]
+        return value
 
-def condition_checker(value):
-    value_l = len(value)
-    if value_l == 0:
-        return f"There wasn`t any olympic games in this year."
-    elif value_l < 10:
-        return f"Ths country got less than 10 medals."
-    return False
+    def condition_checker(self, value):
+        value_l = len(value)
+        if value_l == 0:
+            return f"There wasn`t any olympic games in this year."
+        elif value_l < 10:
+            return f"Ths country got less than 10 medals."
+        return False
 
-def get_list(filename, year):
-    filtered_data_list = []
-    with open(filename, 'r') as file:
-        next_line = file.readline()
-        while next_line != '':
-            data = next_line.split('\t')
-            if get_medal_and_country(data, year):
-                if total is False:
-                    filtered_data_list.append([data[0], data[1], data[6], data[7], data[8], data[9], data[13], data[14]])
-                if total is True:
-                    filtered_data_list.append([data[6], data[14]])
+    def get_list(self):
+        filtered_data_list = []
+        with open(self.data_base, 'r') as file:
             next_line = file.readline()
-    if total is False:
-        return sorted(filtered_data_list, key=lambda x: int(x[0]), reverse=False)
-    return filtered_data_list
+            while next_line != '':
+                data = next_line.split('\t')
+                if self.get_medal_and_country(data):
+                    if not self.total:
+                        filtered_data_list.append([data[0], data[1], data[6], data[7], data[8], data[9], data[13], data[14]])
+                    else:
+                        filtered_data_list.append([data[6], data[14]])
+                next_line = file.readline()
 
-def print_with_total(data_list):
-    data_dict = {}
-    for country, medal in data_list:
-        if country not in data_dict:
-            data_dict[country] = {'gold': 0, 'silver': 0, 'bronze': 0}
-        data_dict[country][medal.lower()] += 1
-    for country, medal in data_dict.items():
-        print(f"{country} - gold:{medal['gold']} - silver:{medal['silver']} - bronze:{medal['bronze']}")
+        if not self.total:
+            return sorted(filtered_data_list, key=lambda x: int(x[0]), reverse=False)
+        return filtered_data_list
 
-def print_without_total(data_list):
-    index = 1
-    print(f"Data for {year} olympic games. Team:{country}.")
-    for data in data_list:
-        name = data[1]
-        discipline = data[6]
-        medal = data[7]
-        print(f"{index}. {name} - {discipline} - {medal}")
-        index += 1
+    def print_with_total(self, data_list):
+        data_dict = {}
+        for country, medal in data_list:
+            if country not in data_dict:
+                data_dict[country] = {'gold': 0, 'silver': 0, 'bronze': 0}
+            data_dict[country][medal.lower()] += 1
+        for country, medal in data_dict.items():
+            print(f"{country} - gold:{medal['gold']} - silver:{medal['silver']} - bronze:{medal['bronze']}")
 
-def print_result(data_list):
-    if not condition_checker(data_list):
-        if total is False:
-            print_without_total(data_list)
-        if total is True:
-            print_with_total(data_list)
-    else:
-        print(condition_checker(data_list))
+    def print_without_total(self, data_list):
+        index = 1
+        print(f"Data for {self.year} olympic games. Team:{self.country}.")
+        for data in data_list:
+            name = data[1]
+            discipline = data[6]
+            medal = data[7]
+            print(f"{index}. {name} - {discipline} - {medal}")
+            index += 1
 
-def main():
-    filtered_data_list = get_list(data_base, year)
-    print_result(filtered_data_list)
+    def print_result(self, data_list):
+        condition = self.condition_checker(data_list)
+        if not condition:
+            if not self.total:
+                self.print_without_total(data_list)
+            else:
+                self.print_with_total(data_list)
+        else:
+            print(condition)
+
+    def main(self):
+        filtered_data_list = self.get_list()
+        self.print_result(filtered_data_list)
 
 
+# ------
 def output_overall(arg_country):
     if arg_country is None:
         return
@@ -114,7 +121,7 @@ class Overall:
     def overall(self):
         with open(args.data_base, 'r') as file:
             line = file.readline()[:-1].split('\t')
-            header = line #створив на всякий випадок, якщо доведеться діставати індекси
+            # header = line #створив на всякий випадок, якщо доведеться діставати індекси
             i_year = 9
             i_medal = -1
             i_country = 7
